738963f96ae0f17ab7de989b38cda4497df2a3fa
fathurDotCom <goatantonio8@gmail.com>
2024-05-31 19:16:08 +0700

    using yaml

--- config/config.go
+++ config/config.go
@@ -2,31 +2,56 @@ package config
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"gopkg.in/yaml.v2"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"log"
-	"os"
-	"github.com/joho/godotenv"
 )
+
+type Config struct {
+	Database struct {
+		Host string `yaml:"host"`
+		User string `yaml:"user"`
+		Password string `yaml:"password"`
+		Name string `yaml:"name"`
+		Port string `yaml:"port"`
+		SSLMode string `yaml:"sslmode"`
+		Timezone string `yaml:"timezone"`
+	} `yaml:"database"`
+}
 
 var DB * gorm.DB
 
-func Connect() {
-	err := godotenv.Load()
+func LoadConfig() (*Config, error) {
+	config := &Config{}
+	data, err := ioutil.ReadFile("config/config.yaml")
 	if err != nil {
-		log.Fatalf("Error loading .env file")
+		return nil, err
 	}
+	err = yaml.Unmarshal(data, config)
+	if err != nil {
+		return nil, err
+	}
+	return config, nil
+}
 
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbName := os.Getenv("DB_NAME")
-	dbSSLMode := os.Getenv("DB_SSLMODE")
-	dbTimezone := os.Getenv("DB_TIMEZONE")
+func Connect() {
+	config, err := LoadConfig()
+	if err != nil {
+		log.Fatalf("Error loading config file: %v", err)
+	}
 	
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
-		dbHost, dbUser, dbPassword, dbName, dbPort, dbSSLMode, dbTimezone)
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
+		config.Database.Host,
+		config.Database.User,
+		config.Database.Password,
+		config.Database.Name,
+		config.Database.Port,
+		config.Database.SSLMode,
+		config.Database.Timezone,
+	)
 
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
